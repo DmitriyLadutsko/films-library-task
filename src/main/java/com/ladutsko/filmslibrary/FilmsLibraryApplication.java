5cbd61791f100dffd260a03db2f69d003c898b32
Dmitriy Ladutsko <dima.ladut@gmail.com>
2021-01-29 19:13:16 +0300

    init commit

---
+++ src/main/java/com/ladutsko/filmslibrary/FilmsLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ladutsko.filmslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmsLibraryApplication.class, args);
+    }
+
+}
